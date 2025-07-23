b26f1327a20109da1f4812a9370b9efe31d6b027
Kurt <jfmanalac@up.edu.ph>
2025-07-23 18:11:23 +0800

    updated for echo commands in script

--- data.sh
+++ data.sh
@@ -7,5 +7,9 @@ node node-api/app.js &
 NODE_PID=$!
 
 wait $FABRIC_CA_PID $NODE_PID
+echo "Server startup finished."
+
+echo "Creating admin folder..."
 mkdir -p $FABRIC_CA_CLIENT_HOME/admin
+echo "admin folder created"
 fabric-ca-client enroll -u http://admin:adminpw@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/admin
