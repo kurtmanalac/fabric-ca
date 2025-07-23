c423b698b933dfa1bb0a468b421557608b59141e
Kurt <jfmanalac@up.edu.ph>
2025-07-23 17:03:03 +0800

    updated wait processes

--- data.sh
+++ data.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-fabric-ca-server start -b admin:adminpw
+fabric-ca-server start -b admin:adminpw &
+FABRIC_CA_PID=$!
 
-node node-api/app.js
+node node-api/app.js &
+NODE_PID=$!
+
+wait $FABRIC_CA_PID $NODE_PID
