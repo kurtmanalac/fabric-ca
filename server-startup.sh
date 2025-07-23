f72db7c6f816514779dd8c5f80cbb3de57d66147
Kurt <jfmanalac@up.edu.ph>
2025-07-23 18:56:06 +0800

    server-startup.sh updated

--- server-startup.sh
+++ server-startup.sh
@@ -3,14 +3,10 @@
 fabric-ca-server start -b admin:adminpw &
 FABRIC_CA_PID=$!
 
-wait $FABRIC_CA_PID 
-
 ./admin-enroll.sh &
 ADMIN_ENROLL_PID=$!
-
-wait $ADMIN_ENROLL_PID
 
 node node-api/app.js &
 NODE_PID=$!
 
-wait $NODE_PID
+wait $FABRIC_CA_PID $NODE_PID $ADMIN_ENROLL_PID
