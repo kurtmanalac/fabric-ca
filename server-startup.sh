9422eea2246c2f0a2af00ed7c936468d9029c323
Kurt <jfmanalac@up.edu.ph>
2025-07-23 18:33:41 +0800

    adjusted order of commands

--- server-startup.sh
+++ server-startup.sh
@@ -6,7 +6,7 @@ FABRIC_CA_PID=$!
 ./admin-enroll.sh &
 ADMIN_ENROLL_PID=$!
 
+wait $FABRIC_CA_PID $ADMIN_ENROLL_PID 
+
 node node-api/app.js &
 NODE_PID=$!
-
-wait $FABRIC_CA_PID $ADMIN_ENROLL_PID $NODE_PID
