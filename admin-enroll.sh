47e76706bb60040aa7c2df2556812d3e3af337a0
Kurt <jfmanalac@up.edu.ph>
2025-07-23 23:39:44 +0800

    admin-enroll.sh checks for admin

--- admin-enroll.sh
+++ admin-enroll.sh
@@ -1,5 +1,6 @@
 echo "Admin enrollment..."
 mkdir -p $FABRIC_CA_CLIENT_HOME/admin/msp
-if 
-fabric-ca-client enroll -u http://admin:adminpw@localhost:7054 --mspdir $FABRIC_CA_CLIENT_HOME/admin/msp
+if fabric-ca-client identity list --id admin
+    fabric-ca-client enroll -u http://admin:adminpw@localhost:7054 --mspdir $FABRIC_CA_CLIENT_HOME/admin/msp
+fi
 echo "Admin enrolled!"
