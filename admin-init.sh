9d9e5d8bf27414f59961242953e6763f3add737f
Kurt <jfmanalac@up.edu.ph>
2025-11-12 17:12:56 +0800

    initial startup file checker and copier

--- admin-init.sh
+++ admin-init.sh
@@ -2,12 +2,22 @@
 
 echo "CA Admin enrollment..."
 mkdir -p $FABRIC_CA_HOME/ca-admin/msp
-if fabric-ca-client identity list --id admin
+if [ -d "$FABRIC_CA_HOME/ca-admin/" ];
 then
     echo "Admin exists!"
 else
     fabric-ca-client enroll -u http://admin:adminpw@localhost:7054 --mspdir $FABRIC_CA_HOME/ca-admin/msp
     echo "Admin enrolled!"
     # copy files to storage
-    
+    SOURCE_URL=${$SOURCE_URL:-http://github-fabric-ca.railway.internal:8000}
+    SOURCE_FOLDER=${SOURCE_FOLDER:-/app/data}
+    FOLDER_NAME=${FOLDER_NAME:-"data"}
+    temp_URL=${temp_URL:-http://fabric-tools-storage.railway.internal:8000}
+    transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg pw "$SOURCE_FOLDER" --arg cmd "$FOLDER_NAME" '{shellScript: $script, envVar: {SOURCE_URL: $url, SOURCE_FOLDER: $folder, FOLDER_NAME: $name}}')
+    echo "Transferring files..."
+    curl -X POST $temp_URL/invoke_script \
+        -H "Content-Type: application/json" \
+        -d "$transfer_json" &
+    TRANSFER_PID=$!
+    wait $TRANSFER_PID
 fi
