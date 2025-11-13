e3db7eca2ae63a2dd3050224b853fc6711dd7a9c
Kurt <jfmanalac@up.edu.ph>
2025-11-13 16:46:18 +0800

    error fixes

--- admin-init.sh
+++ admin-init.sh
@@ -13,7 +13,7 @@ else
     SOURCE_FOLDER=${SOURCE_FOLDER:-/app/data}
     FOLDER_NAME=${FOLDER_NAME:-data}
     temp_URL=${temp_URL:-http://fabric-tools-storage.railway.internal:8000}
-    transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg pw "$SOURCE_FOLDER" --arg cmd "$FOLDER_NAME" '{shellScript: $script, envVar: {SOURCE_URL: $url, SOURCE_FOLDER: $folder, FOLDER_NAME: $name}}')
+    transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg folder "$SOURCE_FOLDER" --arg name "$FOLDER_NAME" '{shellScript: $script, envVar: {SOURCE_URL: $url, SOURCE_FOLDER: $folder, FOLDER_NAME: $name}}')
     echo "Transferring files..."
     curl -X POST $temp_URL/invoke_script \
         -H "Content-Type: application/json" \
--- server-startup.sh
+++ server-startup.sh
@@ -9,15 +9,19 @@ SOURCE_URL=${SOURCE_URL:-http://fabric-tools-storage.railway.internal:8000}
 SOURCE_FOLDER=${SOURCE_FOLDER:-/app/data}
 FOLDER_NAME=${FOLDER_NAME:-data}
 temp_URL=${temp_URL:-http://github-fabric-ca.railway.internal:8000}
-transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg pw "$SOURCE_FOLDER" --arg cmd "$FOLDER_NAME" '{shellScript: $script, envVar: {SOURCE_URL: $url, SOURCE_FOLDER: $folder, FOLDER_NAME: $name}}')
+transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg folder "$SOURCE_FOLDER" --arg name "$FOLDER_NAME" '{shellScript: $script, envVar: {SOURCE_URL: $url, SOURCE_FOLDER: $folder, FOLDER_NAME: $name}}')
 echo "Transferring files..."
 curl -X POST $temp_URL/invoke_script \
     -H "Content-Type: application/json" \
     -d "$transfer_json" &
 TRANSFER_PID=$!
 
+sleep 5
+
 fabric-ca-server start -b admin:adminpw &
 FABRIC_CA_PID=$!
+
+sleep 5
 
 ./admin-init.sh &
 ADMIN_ENROLL_PID=$!
