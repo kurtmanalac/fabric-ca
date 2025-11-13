27b938a2c29574f8ccdb05b2250e76592232e6ae
Kurt <jfmanalac@up.edu.ph>
2025-11-13 19:37:33 +0800

    updated transfer file code in startup and admin

--- server-startup.sh
+++ server-startup.sh
@@ -9,7 +9,7 @@ SOURCE_URL=${SOURCE_URL:-http://fabric-tools-storage.railway.internal:8080}
 SOURCE_FOLDER=${SOURCE_FOLDER:-/app/data}
 FOLDER_NAME=data
 temp_URL=${temp_URL:-http://github-fabric-ca.railway.internal:8000}
-transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg folder "$SOURCE_FOLDER" --arg name "$FOLDER_NAME" '{shellScript: $script, envVar: {SOURCE_URL: $url, SOURCE_FOLDER: $folder, FOLDER_NAME: $name}}')
+transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg folder "$SOURCE_FOLDER" --arg name "$FOLDER_NAME" '{"shellScript": $script, "envVar": {"SOURCE_URL": $url, "SOURCE_FOLDER": $folder, "FOLDER_NAME": $name}}')
 echo "Transferring files..."
 curl -X POST $temp_URL/invoke_script \
     -H "Content-Type: application/json" \
