41020c5806685513f048453d6d698d50b126f340
Kurt <jfmanalac@up.edu.ph>
2025-11-24 17:45:38 +0800

    add static folder

--- node-api/app.js
+++ node-api/app.js
@@ -8,6 +8,7 @@ const fsp = require('fs').promises;
 const { exec } = require('child_process');
 const app = express ();
 app.use(express.json());
+app.use(express.static('app'));
 
 const PORT = process.env.PORT || 3000;
 
