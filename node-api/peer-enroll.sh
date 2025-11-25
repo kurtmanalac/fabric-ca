169a423787408d5dd941fea41acc30594f3add8c
Kurt <jfmanalac@up.edu.ph>
2025-11-26 03:32:07 +0800

    test

--- node-api/peer-enroll.sh
+++ node-api/peer-enroll.sh
@@ -7,7 +7,7 @@ then
 else
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp
     fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW --id.type peer
-    fabric-ca-client getcainfo -u http://localhost:7054 > /app/data/fabric-ca-client/peer1/msp/cacerts/ca-cert.pem
+    fabric-ca-client getcainfo -u http://localhost:7054 > /app/data/fabric-ca-client/peer1/msp/cacerts/github-fabric-ca.railway.internal-7054.pem
     fabric-ca-client enroll -u http://$ENROLL_ID:$ENROLL_PW@localhost:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp $CUSTOM_CMD
     echo "$ENROLL_ID enrolled!"
 fi
