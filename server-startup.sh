1937ec00e116108a0af81ee5622bd021c6b548b6
Kurt <jfmanalac@up.edu.ph>
2025-11-25 21:53:51 +0800

    add server startup file transfer if existing

--- server-startup.sh
+++ server-startup.sh
@@ -5,25 +5,21 @@ NODE_PID=$!
 
 sleep 5
 
-# SOURCE_URL=${SOURCE_URL:-http://fabric-tools-storage.railway.internal:8080}
-# SOURCE_FOLDER=${SOURCE_FOLDER:-/app/data}
-# FOLDER_NAME=data
-# temp_URL=${temp_URL:-http://github-fabric-ca.railway.internal:8000}
-# transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg folder "$SOURCE_FOLDER" --arg name "$FOLDER_NAME" '{"shellScript": $script, "envVar": {"SOURCE_URL": $url, "SOURCE_FOLDER": $folder, "FOLDER_NAME": $name}}')
-# echo "Transferring files..."
-# curl -X POST $temp_URL/invoke-script \
-#     -H "Content-Type: application/json" \
-#     -d "$transfer_json" &
-# TRANSFER_PID=$!
-# wait $TRANSFER_PID
-# sleep 5
-
-fabric-ca-server start -b admin:adminpw &
-FABRIC_CA_PID=$!
+SOURCE_URL=${SOURCE_URL:-http://fabric-tools-storage.railway.internal:8080}
+SOURCE_FOLDER=${SOURCE_FOLDER:-/app/data}
+FOLDER_NAME=data
+temp_URL=${temp_URL:-http://github-fabric-ca.railway.internal:8000}
+transfer_json=$(jq -n --arg script "transfer-file.sh" --arg url "$SOURCE_URL" --arg folder "$SOURCE_FOLDER" --arg name "$FOLDER_NAME" '{"shellScript": $script, "envVar": {"SOURCE_URL": $url, "SOURCE_FOLDER": $folder, "FOLDER_NAME": $name}}')
+echo "Transferring files..."
+curl -X POST $temp_URL/invoke-script \
+    -H "Content-Type: application/json" \
+    -d "$transfer_json"
 
 sleep 5
+
+fabric-ca-server start -b admin:adminpw
 
 ./admin-init.sh &
 ADMIN_ENROLL_PID=$!
 
-wait $NODE_PID $FABRIC_CA_PID $ADMIN_ENROLL_PID
+wait $NODE_PID $ADMIN_ENROLL_PID
