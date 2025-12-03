313fff544baae6c921da8ce0782b228468df0062
Kurt <jfmanalac@up.edu.ph>
2025-12-03 16:23:24 +0800

    test no chmod

--- server-startup.sh
+++ server-startup.sh
@@ -11,7 +11,7 @@ NODE_PID=$!
 mkdir -p /app/postgres
 echo -n | openssl s_client -starttls postgres -connect postgres.railway.internal:5432 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /app/postgres/ca.pem
 sleep 5
-chmod 644 /app/postgres/ca.pem
+#chmod 644 /app/postgres/ca.pem
 sleep 5
 fabric-ca-server start -b admin:adminpw &
 FABRIC_PID=$!
