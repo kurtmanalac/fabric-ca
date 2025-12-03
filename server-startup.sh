3208319f97cd2d1cac522f569cc6172762736cd7
Kurt <jfmanalac@up.edu.ph>
2025-12-03 16:08:52 +0800

    chmod ca file for postgres

--- server-startup.sh
+++ server-startup.sh
@@ -10,7 +10,7 @@ NODE_PID=$!
 
 mkdir -p /app/postgres
 echo -n | openssl s_client -starttls postgres -connect postgres.railway.internal:5432 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /app/postgres/ca.pem
-
+chmod 777 /app/postgres/ca.pem
 fabric-ca-server start -b admin:adminpw &
 FABRIC_PID=$!
 
