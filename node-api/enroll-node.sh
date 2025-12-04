8ff1e795209f7c8830edd0df07aaebc925c9eee2
Kurt <jfmanalac@up.edu.ph>
2025-12-04 21:23:02 +0800

    adjust enroll with sleep

--- node-api/enroll-node.sh
+++ node-api/enroll-node.sh
@@ -7,7 +7,9 @@ then
 else
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp
     fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW --id.type $ID_TYPE
+    sleep 3
     fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
+    sleep 3
     fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/tls --enrollment.profile tls $CUSTOM_CMD --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
     echo "$ENROLL_ID enrolled!"
 fi
