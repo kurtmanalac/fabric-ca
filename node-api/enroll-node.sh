1fb57affaa3c5775eda62bc4533e2d84f5016aa1
Kurt <jfmanalac@up.edu.ph>
2025-12-04 20:33:06 +0800

    adjust enroll node for tls certfile

--- node-api/enroll-node.sh
+++ node-api/enroll-node.sh
@@ -6,7 +6,7 @@ then
     echo "$ENROLL_ID exists!"
 else
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp
-    fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW --id.type $ID_TYPE
+    fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW --id.type $ID_TYPE --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
     fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp $CUSTOM_CMD
     echo "$ENROLL_ID enrolled!"
 fi
