3e6cef7a84995324639d4c354a792600a92dada4
Kurt <jfmanalac@up.edu.ph>
2025-12-04 20:54:48 +0800

    enroll tls profile

--- node-api/enroll-node.sh
+++ node-api/enroll-node.sh
@@ -7,6 +7,7 @@ then
 else
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp
     fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW --id.type $ID_TYPE
-    fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp $CUSTOM_CMD --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
+    fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
+    fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/tls --enrollment.profile tls $CUSTOM_CMD --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
     echo "$ENROLL_ID enrolled!"
 fi
