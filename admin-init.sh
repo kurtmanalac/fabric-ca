bb4ffeadab6a7f1e75a5cd38f74b2d7883a7d7be
Kurt <jfmanalac@up.edu.ph>
2025-12-08 16:47:22 +0800

    change filepath

--- admin-init.sh
+++ admin-init.sh
@@ -9,7 +9,7 @@ else
     fabric-ca-client enroll -u https://admin:adminpw@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_HOME/ca-admin/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
     # find /app/data/fabric-ca/ca-admin/msp/keystore/ -type f -name '*_sk' -exec rm {} \;
     fabric-ca-client enroll -u https://admin:adminpw@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_HOME/ca-admin/msp --csr.hosts $RAILWAY_PRIVATE_DOMAIN,$RAILWAY_SERVICE_NAME --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem --enrollment.profile tls
-    find /app/data/fabric-ca-server/msp/keystore/ -type f -name '*_sk' -print0 | xargs -0 ls -t | tail -n +2 | xargs rm -f
+    find /app/data/fabric-ca/ca-admin/msp/keystore/ -type f -name '*_sk' -print0 | xargs -0 ls -t | tail -n +2 | xargs rm
     echo "Admin enrolled!"
     mkdir -p $FABRIC_CA_HOME/ca-admin/msp/tlscacerts
     # cp $FABRIC_CA_SERVER_HOME/tls-cert.pem $FABRIC_CA_HOME/ca-admin/msp/tlscacerts/tls-cert.pem
