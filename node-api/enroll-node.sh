993b0055dde7d9d9cc6f691bce8a036f2e431248
Kurt <jfmanalac@up.edu.ph>
2025-12-08 16:40:19 +0800

    add tls client files

--- node-api/enroll-node.sh
+++ node-api/enroll-node.sh
@@ -7,7 +7,8 @@ then
 else
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp
     FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca/ca-admin/msp}
-    fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW -u https://github-fabric-ca.railway.internal:7054 --id.type $ID_TYPE
+    KEYFILE=$(find /app/data/fabric-ca/ca-admin/msp/keystore -type f -name '*_sk')
+    fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW -u https://github-fabric-ca.railway.internal:7054 --id.type $ID_TYPE --tls.client.certfile /app/data/fabric-ca/ca-admin/signcerts/cert.pem --tls.client.keyfile /app/data/fabric-ca/ca-admin/msp/keystore/$KEYFILE
     sleep 5
     FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
     fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
