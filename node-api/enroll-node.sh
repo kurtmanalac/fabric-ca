8eae4ecfd11d3709fd1cad26be39403b4bf09acb
Kurt <jfmanalac@up.edu.ph>
2025-12-08 14:47:34 +0800

    change filepath

--- node-api/enroll-node.sh
+++ node-api/enroll-node.sh
@@ -6,8 +6,9 @@ then
     echo "$ENROLL_ID exists!"
 else
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp
-    FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-server/msp}
+    FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca/ca-admin/msp}
     fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW -u https://github-fabric-ca.railway.internal:7054 --id.type $ID_TYPE
+    sleep 5
     FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
     # sleep 5
     # fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
