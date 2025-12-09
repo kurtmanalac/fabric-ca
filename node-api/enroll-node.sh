b311647bd4da662ec7904f5d6a3e7040eb4b42c4
Kurt <jfmanalac@up.edu.ph>
2025-12-09 10:19:30 +0800

    test fix

--- node-api/enroll-node.sh
+++ node-api/enroll-node.sh
@@ -11,8 +11,8 @@ else
     fabric-ca-client register --id.name $ENROLL_ID --id.secret $ENROLL_PW -u https://github-fabric-ca.railway.internal:7054 --id.type $ID_TYPE --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem --tls.client.certfile /app/data/fabric-ca/ca-admin/tls/signcerts/cert.pem --tls.client.keyfile $KEYFILE
     sleep 5
     FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
-    fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem
     fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/tls --enrollment.profile tls --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem $CUSTOM_CMD
+    fabric-ca-client enroll -u https://$ENROLL_ID:$ENROLL_PW@github-fabric-ca.railway.internal:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp --tls.certfiles /app/data/fabric-ca-server/tls-cert.pem $CUSTOM_CMD
     mkdir -p $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp/tlscacerts
     cp $FABRIC_CA_SERVER_HOME/tls-cert.pem $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp/tlscacerts/tls-cert.pem
     mv $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp/cacerts/github-fabric-ca-railway-internal-7054.pem $FABRIC_CA_CLIENT_HOME/$ENROLL_ID/msp/cacerts/ca-cert.pem
