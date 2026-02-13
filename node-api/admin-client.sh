600855cdd7657cac8e4f3accf6034bf482512c16
Kurt <jfmanalac@up.edu.ph>
2026-02-13 16:28:59 +0800

    update admin client script

--- node-api/admin-client.sh
+++ node-api/admin-client.sh
@@ -2,7 +2,7 @@ CA_URL=${CA_URL:-http://github-fabric-ca.railway.internal:8000}
 CLIENT=${CLIENT:-upmoadmin}
 CLIENT_PW=${CLIENT_PW:-upmomadminpw}
 customCmd=${customCmd:---csr.hosts $RAILWAY_SERVICE_NAME,$RAILWAY_PRIVATE_DOMAIN --csr.names C=US,ST=California,L=SanFrancisco,O=upmo,OU=admin}
-admin_json=$(jq -n --arg script "enroll-client.sh" --arg client "$CLIENT" --arg clientpw "$CLIENTPW" --arg cmd "$customCmd" --arg type "admin" '{"shellScript": $script, "envVar": {"CLIENT": $client, "CLIENT_PW": $clientpw, "ID_TYPE": $type, "CUSTOM_CMD": $cmd}}')
+admin_json=$(jq -n --arg script "enroll-client.sh" --arg client "$CLIENT" --arg clientpw "$CLIENT_PW" --arg cmd "$customCmd" --arg type "admin" '{"shellScript": $script, "envVar": {"CLIENT": $client, "CLIENT_PW": $clientpw, "ID_TYPE": $type, "CUSTOM_CMD": $cmd}}')
 curl -X POST $CA_URL/invoke-script \
     -H "Content-Type: application/json" \
     -d "$admin_json"
